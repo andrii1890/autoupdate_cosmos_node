0925fcf484c89fd1c87b6e6b7748f87454935b99
st.Game | acloud.pp.ua <95629373+andrii1890@users.noreply.github.com>
2022-10-04 21:08:46 +0300

    Create sei_devnet.sh

---
+++ sei_devnet.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+GREEN_COLOR='\033[0;32m'
+RED_COLOR='\033[0;31m'
+NO_COLOR='\033[0m'
+BLOCK=8871251
+VERSION=1.2.2beta
+echo -e "$GREEN_COLOR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
+for((;;)); do
+	height=$(seid status |& jq -r ."SyncInfo"."latest_block_height")
+	if ((height>=$BLOCK)); then
+
+		systemctl stop seid.service
+		mv /home/sei/seid /usr/local/bin/
+		echo "restart the node..."
+		systemctl restart seid.service
+
+		for (( timer=60; timer>0; timer-- )); do
+			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
+			sleep 1
+		done
+		height=$(seid status |& jq -r ."SyncInfo"."latest_block_height")
+		if ((height>$BLOCK)); then
+			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
+		fi
+		seid version --long | head
+		break
+	else
+		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
+	fi
+	sleep 5
+done
